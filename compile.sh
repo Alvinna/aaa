d7080b32904a45b93f8e44fdaca527c5ba3d81dd
Clara <clarachang51201@gmail.com>
2026-03-08 18:20:28 -0700

    fixes

--- compile.sh
+++ compile.sh
@@ -10,5 +10,6 @@ do
     pandoc "$file" -o public/$fn.html --lua-filter=links-to-html.lua --template template.html --include-in-header header.html --include-before-body navbar.html --include-after-body footer.html --standalone --no-highlight --toc --toc-depth 2 --mathjax
 done
 IFS="$OIFS"
+cp *.css public/
 
 
